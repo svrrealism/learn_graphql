60aaeb962e2c60c1790c093bf967c20a6feac979
svrrealism <svrrealists@gmail.com>
2018-02-15 21:20:07 -0500

    Create movie, query, and mutation type

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,11 +1,15 @@
 Types::MutationType = GraphQL::ObjectType.define do
   name "Mutation"
 
-  # TODO: Remove me
-  field :testField, types.String do
-    description "An example field added by the generator"
-    resolve ->(obj, args, ctx) {
-      "Hello World!"
+  field :createMovie, Types::MovieType do
+    argument :title, !types.String
+    argument :description, !types.String
+
+    resolve -> (obj, args, ctx) {
+      Movie.create(
+        title: args[:title],
+        description: args[:description]
+      )
     }
   end
 end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,11 +3,22 @@ Types::QueryType = GraphQL::ObjectType.define do
   # Add root-level fields here.
   # They will be entry points for queries on your schema.
 
-  # TODO: remove me
-  field :testField, types.String do
-    description "An example field added by the generator"
-    resolve ->(obj, args, ctx) {
-      "Hello World!"
+  field :allMovies do
+    type types[Types::MovieType]
+    description "Returns all of the movies"
+
+    resolve -> (obj, args, ctx) {
+      Movie.all
+    }
+  end
+
+  field :movie do
+    type Types::MovieType
+    description "Return a movie"
+    argument :id, !types.ID
+
+    resolve -> (obj, args, ctx) {
+      Movie.find(args[:id])
     }
   end
 end
